58e90a34b0984172094b185a4b3ea11647e9bc5b
gabikeane <gabikeane@pitt.edu>
2017-10-07 17:59:29 -0400

    some more sandbox mapping and bit of TEI

---
+++ sandbox/initialtimesmapjavascript/data/initialtimesdata_0.js
@@ -0,0 +1,13 @@
+var json_initialtimesdata_0 = {
+"type": "FeatureCollection",
+"name": "initialtimesdata_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": 2589581.0, "X": 28.0, "Y": 3.0, "Count": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 3.0, 28.0 ] } },
+{ "type": "Feature", "properties": { "ID": 3117735.0, "X": 40.4165, "Y": -3.70256, "Count": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -3.70256, 40.4165 ] } },
+{ "type": "Feature", "properties": { "ID": 1275004.0, "X": 22.56263, "Y": 88.36304, "Count": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 88.36304, 22.56263 ] } },
+{ "type": "Feature", "properties": { "ID": 1273294.0, "X": 28.65195, "Y": 77.23149, "Count": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 77.23149, 28.65195 ] } },
+{ "type": "Feature", "properties": { "ID": 1269750.0, "X": 22.0, "Y": 79.0, "Count": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 79.0, 22.0 ] } },
+{ "type": "Feature", "properties": { "ID": 1259224.0, "X": 30.0, "Y": 74.0, "Count": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 74.0, 30.0 ] } }
+]
+}
